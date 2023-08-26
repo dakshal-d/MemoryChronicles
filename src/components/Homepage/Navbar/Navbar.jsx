3c841a020faf29d4eb6cd7aa15a2f2db3fa6cfb1
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-08-26 15:36:53 +0530

    Menubar Updated

--- src/components/Homepage/Navbar/Navbar.jsx
+++ src/components/Homepage/Navbar/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
 
 
                 {/* Profile dropdown */}
-                {(auth) && (<Menu as="div" className="relative ml-3 animate-pulse">
+                {(auth) && (<Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
